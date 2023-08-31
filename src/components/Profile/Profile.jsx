47ab9b8baa16f248381d8bc715194589a62d70b0
Rumiya Kurmangazieva <kurmangazieva_2001@mail.ru>
2023-09-01 00:16:41 +0300

    исправила ошибку отправки  невалидных данных

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,100 +1,120 @@
-import { useState, useContext, useEffect } from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import CurrentUserContext from "../../context/CurrentUserContext";
 import Navigation from "../Navigation/Navigation";
+import useInput from "../../hooks/useInput";
 
 import "./Profile.css";
 
 function Profile({ updateUser, onSignOut }) {
   const currentUser = useContext(CurrentUserContext);
-  const userName = currentUser.data ? currentUser.data.name : '';
-  const userEmail = currentUser.data ? currentUser.data.email : '';
+  const userName = currentUser.data ? currentUser.data.name : "";
+  const userEmail = currentUser.data ? currentUser.data.email : "";
 
-  const [formValue, setFormValue] = useState({
-    email: userEmail,
-    name: userName,
+  const nameValidation = useInput(userName, {
+    isEmpty: true,
+    minLength: 2,
+    maxLength: 30,
   });
 
-  const [accessEdit, setAccessEdit] = useState(true);
-  const [buttonDisable, setButtonDisable] = useState(false);
+  const emailValidation = useInput(userEmail, { isEmpty: true, isEmail: true });
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
+  const validMinName = () =>
+    nameValidation.hasChanged && nameValidation.minLengthErr;
 
-    setFormValue({
-      ...formValue,
-      [name]: value,
-    });
+  const validMaxName = () =>
+    nameValidation.hasChanged && nameValidation.maxLengthErr;
+
+  const validEmail = () =>
+    emailValidation.hasChanged && emailValidation.emailErr;
+
+  const validEmpty = () => {
+    return (
+      (nameValidation.validEmpty && nameValidation.isEmpty) ||
+      (emailValidation.hasChanged && emailValidation.isEmpty)
+    );
   };
+
+  const handleDisableButton = () =>
+    (!nameValidation.inputValid || nameValidation.value === userName) &&
+    (!emailValidation.inputValid || emailValidation.value === userEmail);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    updateUser(formValue);
-    toggleAccessEdit();
+    const name = nameValidation.value;
+    const email = emailValidation.value;
+    updateUser({ name, email });
   };
-
-  const toggleAccessEdit = () => {
-    setAccessEdit(!accessEdit);
-  };
-
-  useEffect(() => {
-    if (formValue.email === userEmail && formValue.name === userName) {
-      return setButtonDisable(true);
-    } else {
-      return setButtonDisable(false);
-    }
-  }, [formValue.email, formValue.name, userEmail, userName]);
 
   return (
     <div className="profile">
       <Navigation logined />
       <div className="profile__container">
         <h2 className="profile__title">{`Привет, ${userName}!`}</h2>
-        <div className="profile__info">
-          <div className="profile__input-container">
-            <span className="profile__span">Имя</span>
-            <input
-              className="profile__input"
-              type="text"
-              placeholder="Введите имя"
-              name="name"
-              value={formValue.name}
-              disabled={accessEdit}
-              onChange={handleChange}
-            />
+        <form onSubmit={handleSubmit}>
+          <div className="profile__info">
+            <div className="profile__input-container">
+              <span className="profile__span">Имя</span>
+              <input
+                className="profile__input"
+                type="text"
+                placeholder="Введите имя"
+                name="name"
+                onBlur={nameValidation.onBlur}
+                onChange={nameValidation.onChange}
+                value={nameValidation.value}
+              />
+            </div>
+            <div className="profile__input-container">
+              <span className="profile__span">E-mail</span>
+              <input
+                className="profile__input"
+                type="email"
+                placeholder="Введите почту"
+                name="email"
+                onBlur={emailValidation.onBlur}
+                onChange={emailValidation.onChange}
+                value={emailValidation.value}
+              />
+            </div>
+            {validMinName() && (
+              <span className="profile__error">
+                Имя должно содержать не менее 2 символов
+              </span>
+            )}
+            {validMaxName() && (
+              <span className="profile__error">
+                Имя должно содержать не более 30 символов
+              </span>
+            )}
+            {validEmail() && (
+              <span className="profile__error">Введен некорректный e-mail</span>
+            )}
+            {validEmpty() && (
+              <span className="profile__error">
+                Необходимо заполнить все поля
+              </span>
+            )}
           </div>
-          <div className="profile__input-container">
-            <span className="profile__span">E-mail</span>
-            <input
-              className="profile__input"
-              type="email"
-              placeholder="Введите почту"
-              name="email"
-              value={formValue.email}
-              disabled={accessEdit}
-              onChange={handleChange}
-            />
+          <div className="profile__buttons-container">
+            <button
+              className="profile__button"
+              disabled={handleDisableButton()}
+              type="submit"
+            >
+              Редактировать
+            </button>
+            <Link
+              className="profile__button"
+              to="/"
+              style={{ color: "#EE3465" }}
+              onClick={onSignOut}
+            >
+              Выйти из аккаунта
+            </Link>
           </div>
-        </div>
-        <div className="profile__buttons-container">
-          <Link
-            className={`profile__button ${
-              !accessEdit && buttonDisable ? "profile__button_disabled" : ""
-            }`}
-            onClick={accessEdit ? toggleAccessEdit : handleSubmit}
-          >
-            {accessEdit ? "Редактировать" : "Изменить"}
-          </Link>
-          <Link
-            className="profile__button"
-            to="/"
-            style={{ color: "#EE3465" }}
-            onClick={onSignOut}
-          >
-            Выйти из аккаунта
-          </Link>
-        </div>
+        </form>
       </div>
     </div>
   );
